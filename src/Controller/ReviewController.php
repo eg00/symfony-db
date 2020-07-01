9029234820c5d9fed50b4860dbcfdbfa5e910fb5
Egor Povshok <egor@povshok.ru>
2020-07-01 07:05:02 +0500

    Reviews
    implements update method ReviewController

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Reviews;
+use App\Form\ReviewType;
 use App\Service\BookManagerInterface;
 use App\Service\ReviewManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +25,7 @@ class ReviewController extends AbstractController
     /**
      * @Route("/book/show/{bookId<\d+>}/reviews", name="reviews")
      *
-     * @param int $page
+     * @param int $bookId
      *
      * @return Response
      */
@@ -39,6 +42,7 @@ class ReviewController extends AbstractController
     /**
      * @Route("/book/show/{bookId<\d+>}/review/{id<\d+>}", name="review_show")
      *
+     * @param int $bookId
      * @param int $id
      *
      * @return Response
@@ -49,11 +53,72 @@ class ReviewController extends AbstractController
 
         return $this->render('review/show.html.twig', [
             'review' => $this->reviewManager->get($id),
-            'bookId' => $bookId
+            'bookId' => $bookId,
         ]);
     }
 
-    private function checkIfBookExists($bookId)
+    /**
+     * @Route("/book/show/{bookId<\d+>}/review/create", name="review_create")
+     *
+     * @param int     $bookId
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function create(int $bookId, Request $request): Response
+    {
+        $this->checkIfBookExists($bookId);
+
+        $review = new Reviews();
+        $form = $this->createForm(ReviewType::class, $review,
+            ['attr' => ['bookId' => $bookId],
+            ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review = $form->getData();
+
+            $this->reviewManager->store($review);
+
+            return $this->redirectToRoute('book_show', ['id' => $bookId]);
+        }
+
+        return $this->render('review/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/book/show/{bookId<\d+>}/review/update/{id<\d+>}", name="review_update")
+     *
+     * @param int     $bookId
+     * @param int     $id
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function update(int $bookId, int $id, Request $request): Response
+    {
+        $this->checkIfBookExists($bookId);
+        $review = $this->reviewManager->get($id);
+        $form = $this->createForm(ReviewType::class, $review,
+            ['attr' => ['bookId' => $bookId],
+            ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review = $form->getData();
+            $this->reviewManager->update($review);
+
+            return $this->redirectToRoute('book_show', ['id' => $bookId]);
+        }
+
+        return $this->render('book/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    private function checkIfBookExists($bookId): void
     {
         if (!$this->bookManager->get($bookId)) {
             throw $this->createNotFoundException('The book does not exist');
--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -3,7 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Reviews;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,13 +17,25 @@ class ReviewType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
-            ->add('book_id')
-            ->add('reviever_name')
-            ->add('content')
-            ->add('rating')
-            ->add('published_date')
-            ->add('book')
+            ->add('book_id', HiddenType::class,
+                [
+                'data' => $options['attr']['bookId'],
+            ]
+            )
+            ->add('reviever_name', TextType::class)
+            ->add('content', TextType::class)
+            ->add('rating', ChoiceType::class, [
+                'choices' => array_combine(range(1,10), range(1,10)),
+            ])
+            ->add('published_date', DateTimeType::class, [
+                'data' => new \DateTime(),
+                'attr' => array(
+                    'readonly' => true,
+                ),
+            ])
+            ->add('save', SubmitType::class);
         ;
     }
 
